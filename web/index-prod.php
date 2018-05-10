c41a284758518c6e5d536dce1c675d21c5437618
sungd@jiedaibao.com <sungd@jiedaibao.com>
2018-05-11 02:48:37 +0800

    fix

--- web/index-prod.php
+++ web/index-prod.php
@@ -1,25 +1,18 @@
-#!/usr/bin/env php
 <?php
-/**
- * Yii console bootstrap file.
- *
- * @link http://www.yiiframework.com/
- * @copyright Copyright (c) 2008 Yii Software LLC
- * @license http://www.yiiframework.com/license/
- */
 
-// fcgi doesn't have STDIN and STDOUT defined by default
-defined('STDIN') or define('STDIN', fopen('php://stdin', 'r'));
-defined('STDOUT') or define('STDOUT', fopen('php://stdout', 'w'));
+// comment out the following two lines when deployed to production
+defined('YII_ENV') or define('YII_ENV', 'prod');
 
-require(__DIR__ . '/vendor/autoload.php');
-require(__DIR__ . '/vendor/yiisoft/yii2/Yii.php');
-require(__DIR__ . '/web/ConfigManager.php');
+require(__DIR__ . '/../vendor/autoload.php');
+require(__DIR__ . '/../vendor/yiisoft/yii2/Yii.php');
 
-$config1 = require(__DIR__ . '/config/prod/console.php');
-$config = app\web\ConfigManager::loadConfig($config1,'/data/conf/aeolus/server.ini');
+$config = require(__DIR__ . '/../config/prod/web.php');
 
-$application = new yii\console\Application($config);
+$application = new yii\web\Application($config);
+
+Yii::setAlias('@tmp',  '@runtime/tmp');
 Yii::setAlias('@data',  '@runtime/data');
-$exitCode = $application->run();
-exit($exitCode);
+$application->language='zh-CN';
+$application->defaultRoute='admin';
+$application->run();
+
