9a519788b7396bff2243169d987e0fe4b82eecc7
sungd@jiedaibao.com <sungd@jiedaibao.com>
2018-05-12 03:45:01 +0800

    fix

--- config/prod/web.php
+++ config/prod/web.php
@@ -6,9 +6,9 @@ $params = require(__DIR__ . '/params.php');
 
 $config = [
     'id' => 'basic',
-    'basePath' => dirname(__DIR__),
+    'basePath' => dirname(dirname(__DIR__)),
     'bootstrap'  => ['log'],
-    'modules'    => [],
+    'timeZone'  => 'PRC',
     'components' => \yii\helpers\ArrayHelper::merge ([
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
