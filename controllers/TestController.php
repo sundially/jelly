c2aec0004fcab63f383df45b41ee70ccb9b78ec9
sungd@jiedaibao.com <sungd@jiedaibao.com>
2018-05-12 03:47:47 +0800

    fix

--- controllers/TestController.php
+++ controllers/TestController.php
@@ -15,8 +15,8 @@ class TestController extends Controller
 
     public function actionIndex(){
         $request = \Yii::$app->getRequest()->get();
-        var_dump('：你在哪呢？'.PHP_EOL);
-        var_dump('：在外边'.PHP_EOL);
+        echo ('：你在哪呢？'.PHP_EOL);
+        echo ('：在外边'.PHP_EOL);
         \Yii::$app->params;
     }
     /**
