1c65eb6ef727edb873fa952a446c547f59ab37b3
sungd@jiedaibao.com <sungd@jiedaibao.com>
2018-05-17 16:18:53 +0800

    fix

--- config/local/db.php
+++ config/local/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=mysql',
+    'dsn' => 'mysql:host=localhost;dbname=dmp',
     'username' => 'root',
     'password' => '123456',
     'charset' => 'utf8',
--- config/prod/db.php
+++ config/prod/db.php
@@ -2,8 +2,8 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=127.0.0.1;dbname=mysql',
-    'username' => 'root',
-    'password' => 'Sungd@mysql1234*',
+    'dsn' => 'mysql:host=127.0.0.1;dbname=blog',
+    'username' => '',
+    'password' => '',
     'charset' => 'utf8',
 ];
